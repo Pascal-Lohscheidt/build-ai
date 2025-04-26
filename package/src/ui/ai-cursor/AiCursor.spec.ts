fa1ca2fe0329b3906950758acadb570a4154f73c
Pascal Lohscheidt <pascal.lohscheidt@gmail.com>
2025-04-26 16:47:11 +0200

    test: added first real test setup for visual components

--- package/src/ui/ai-cursor/AiCursor.spec.ts
+++ package/src/ui/ai-cursor/AiCursor.spec.ts
@@ -1,7 +1,57 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
+import { AiCursor } from './AiCursor';
 
-describe('AiCursor', () => {
-  it('should be a function', () => {
-    expect(true).toBe(true);
-  });
-});
+/**
+ * At the moment, the test setup is not very good.
+ * We will write some basic tests to cover the most important functionality.
+ */
+describe(
+  'AiCursor',
+  {
+    timeout: 10_000,
+  },
+  () => {
+    beforeEach(() => {
+      window.document.body.innerHTML = '';
+    });
+
+    it('spawn() should create a new AiCursor instance', () => {
+      expect(window.document.body.querySelectorAll('ai-cursor')).toHaveLength(
+        0
+      );
+
+      AiCursor.spawn();
+
+      const aiCursor = window.document.body.querySelector('ai-cursor');
+      expect(aiCursor).toBeDefined();
+    });
+
+    it('spawn() and move() the cursor to a position', async () => {
+      const cursor = AiCursor.spawn();
+
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      cursor.moveTo([100, 100]);
+
+      await new Promise((resolve) => setTimeout(resolve, 1_000));
+
+      const aiCursor = window.document.body.querySelector('ai-cursor');
+      expect(aiCursor).toBeDefined();
+
+      const span = aiCursor?.shadowRoot?.querySelector(
+        '#cursor-graphic-parent'
+      );
+
+      // since the mouse cursor uses css to animate the position, we need to check the styling in JSDom
+      // it is questionable if this a very valuable test.
+
+      //translateX(100px) translateY(100px)
+      const regex = /translateX\(([^)]+)px\)\s+translateY\(([^)]+)px\)/;
+      const match = span?.getAttribute('style')?.match(regex);
+      expect(match).toBeDefined();
+
+      const [, translateX, translateY] = match!;
+      expect(translateX).toBeCloseTo(100);
+      expect(translateY).toBeCloseTo(100);
+    });
+  }
+);
